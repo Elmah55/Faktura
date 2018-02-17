e2a5523cb47cec0291ae498abf6e19fe98a03182
Elmah <elmah.michal@gmail.com>
2018-02-17 22:51:36 +0100

    Add PDFPageEventClass

---
+++ Faktura/Invoices/PDFPageEvent.cs
@@ -0,0 +1,21 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Faktura.Invoices
+{
+    /// <summary>
+    /// This is class contaings C# style events for iTextSharp page events.
+    /// </summary>
+    class PDFPageEvent : PdfPageEventHelper, IPdfPageEvent
+    {
+        public event PDFPageEventHandler EndPage;
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            EndPage?.Invoke(document, writer);
+            base.OnEndPage(writer, document);
+        }
+    }
+
+    public delegate void PDFPageEventHandler(Document document, PdfWriter writer);
+}
